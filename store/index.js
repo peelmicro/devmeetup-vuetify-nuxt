b28d2415d7c828aba3dd4dffe419b5acf21022b8
RIM\Juan.Pablo.Perez <juan.pablo@retailinmotion.com>
2018-03-21 19:06:28 +0000

    Create Meetup

--- store/index.js
+++ store/index.js
@@ -24,8 +24,25 @@ const createStore = () => {
         registeredMeetups: ['3asd342342342']
       }
     },
-    mutations: {},
-    actions: {},
+    mutations: {
+      createMeetup (state, payload) {
+        state.loadedMeetups.push(payload)
+      }
+    },
+    actions: {
+      createMeetup ({commit}, payload) {
+        const meetup = {
+          title: payload.title,
+          location: payload.location,
+          imageUrl: payload.imageUrl,
+          description: payload.description,
+          date: payload.date,
+          id: '34dw634rr664'
+        }
+        commit('createMeetup', meetup)
+        // reach out to firebase and store it
+      }
+    },
     getters: {
       loadedMeetups (state) {
         return state.loadedMeetups.sort((meetupA, meetupB) => {
