297e00a58222e710744929dcd5e91c1e6c88efaa
RIM\Juan.Pablo.Perez <juan.pablo@retailinmotion.com>
2018-03-30 10:01:22 +0100

    Fetching User State

--- plugins/check-already-auth.js
+++ plugins/check-already-auth.js
@@ -4,6 +4,7 @@ export default function ({store, router}) {
   firebaseApp.auth().onAuthStateChanged((user) => {
     if (user) {
       store.dispatch('autoSignIn', user)
+      store.dispatch('fetchUserData')
     }
   })
 }
--- store/index.js
+++ store/index.js
@@ -223,6 +223,31 @@ const createStore = () => {
           fbKeys: {}
         })
       },
+      fetchUserData  ({ commit, getters }) {
+        commit('setLoading', true)
+        firebase.database().ref('/users/' + getters.user.id + '/registrations/')
+          .once('value')
+          .then(data => {
+            const dataPairs = data.val()
+            let registeredMeetups = []
+            let swappedPairs = {}
+            for (let key in dataPairs) {
+              registeredMeetups.push(dataPairs[key])
+              swappedPairs[dataPairs[key]] = key
+            }
+            const updatedUser = {
+              id: getters.user.id,
+              registeredMeetups: registeredMeetups,
+              fbKeys: swappedPairs
+            }
+            commit('setLoading', false)
+            commit('setUser', updatedUser)
+          })
+          .catch(error => {
+            commit('setLoading', false)
+            console.log(error)
+          })
+      },
       logout ({ commit }) {
         firebase.auth().signOut()
         commit('setUser', null)
